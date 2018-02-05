724db4004b906cf5c7e0b88ed2fe7dd477ce4d48
Tom Chappell <tom.chappell@biogrammatics.com>
2018-02-05 09:09:41 -0800

    5 February 2018

--- pubmed.rb
+++ pubmed.rb
@@ -65,7 +65,7 @@ unless (oldids == newids)
     bib.match(link)
     linktext = "<p><a href=" + $1.to_s + ">pubmed link</a></p>"
 
-    File.open(html_file, 'a') {|f| f.write("<hr><h4>" + titletext + "</h4>" + "<p>" + trends + "</p>" + linktext + "<details><summary>Abstract</summary>" + abstracttext + "</details>") }
+    File.open(html_file, 'a') {|f| f.write("<div itemscope itemtype=\"http://schema.org/ScholarlyArticle\"><hr><h4><span itemprop=\"name\">" + titletext + "</span></h4>" + "<p>" + trends + "</p>" + linktext + "<details><summary>Abstract</summary>" + abstracttext + "</details></div>\n") }
 
   end
 
